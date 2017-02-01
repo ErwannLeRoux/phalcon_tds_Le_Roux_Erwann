9ef37312db08dfcd6456adf1a7c6cde043f5cdb3
Erwann.leroux <erwann.leroux1@gmail.com>
2017-02-01 15:05:07 +0100

    Début TD1

---
+++ TD1/cache/c__wamp64_www_phalcon_tds_le_roux_erwann_td1_app_views_index_index.volt.php
@@ -0,0 +1,12 @@
+<div class="page-header">
+    <h1>Congratulations!</h1>
+</div>
+
+<p>You're now flying with Phalcon. Great things are about to happen!</p>
+
+<p>This page is located at <code>views/index/index.volt</code></p>
+
+<h1>Hello world !</h1>
+
+<?= $this->tag->linkto('inscription', 'Inscrivez-vous...') ?>
+
